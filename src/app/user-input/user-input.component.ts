a4b3adba89116ddc1dfcc82ac25aef4d70edf930
Kaylin Maharaj <kaylinmaharaj@gmail.com>
2026-07-16 19:37:08 +0200

    feat: Add investment calculation functionality and results display

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  enteredInitialInvestment: string = '10000';
+  enteredAnnualInvestment: string = '10';
+  enteredExpectedReturn:string = '5';
+  enteredDuration:string = '5'
+
+
+  onSubmit(){
+    this.calculate.emit(
+      {
+        initialInvestment: +this.enteredInitialInvestment,
+        duration: +this.enteredDuration,
+        expectedReturn: +this.enteredExpectedReturn,
+        annualInvestment: +this.enteredAnnualInvestment
+      }
+    );
+  }
 
 }
